b0c4ff26c50a6c0114158bcb06f9e938853ce32a
klauth86 <kehel@yandex.ru>
2022-10-19 21:47:05 +0200

    Add NOTICE to source

--- InputSeqProj_4_26_02/Plugins/InputSequence/Source/InputSequenceEditor/Private/Graph/InputSequenceGraphSchema.cpp
+++ InputSeqProj_4_26_02/Plugins/InputSequence/Source/InputSequenceEditor/Private/Graph/InputSequenceGraphSchema.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022. Pentangle Studio. All rights reserved.
+// Copyright 2022 Pentangle Studio Licensed under the Apache License, Version 2.0 (the «License»);
 
 #include "Graph/InputSequenceGraphSchema.h"
 #include "Graph/InputSequenceGraph.h"
