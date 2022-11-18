cb96a180318c0ef0d9beaa1ec0eb8205366672d4
klauth86 <kehel@yandex.ru>
2022-11-18 20:50:48 +0100

    update plugin meta

--- InputSeqProj_5_01_00/Packaging/package_plugin.bat
+++ InputSeqProj_5_01_00/Packaging/package_plugin.bat
@@ -1,1 +1,1 @@
-"D:\UE_5.0\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="D:\UE_Projects\UE_Plugin_InputSequence\InputSeqProj_5_00_03\Plugins\InputSequence\InputSequence.uplugin" -Package="D:\UE_Projects\UE_Plugin_InputSequence\InputSeqProj_5_00_03\Packaging\Tmp" -Rocket -VS2022
+"D:\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin="D:\UE_Projects\UE_Plugin_InputSequence\InputSeqProj_5_01_00\Plugins\InputSequence\InputSequence.uplugin" -Package="D:\UE_Projects\UE_Plugin_InputSequence\InputSeqProj_5_01_00\Packaging\Tmp" -Rocket -VS2022
