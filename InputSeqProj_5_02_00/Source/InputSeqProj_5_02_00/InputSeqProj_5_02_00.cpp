a32e6df5e55d45514a7ed7bba9715f9ba1b96746
klauth86 <kehel@yandex.ru>
2023-05-12 20:01:08 +0200

    Initial commit

---
+++ InputSeqProj_5_02_00/Source/InputSeqProj_5_02_00/InputSeqProj_5_02_00.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InputSeqProj_5_02_00.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InputSeqProj_5_02_00, "InputSeqProj_5_02_00" );
